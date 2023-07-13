7aa6c385818797f9e021ec4ad64e004719ac2c44
sharifmrahat <sharifmrahat@gmail.com>
2023-07-13 23:20:55 +0600

    module-22 is added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
+import { api } from './api/apiSlice';
+import userReducer from './features/user/userSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    product: productReducer,
+    user: userReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
